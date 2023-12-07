831605c3d188e0f302735f22462e45087fd0693c
StormRobert <stormarasa2@gmail.com>
2023-12-08 01:31:11 +0300

    first

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount =0):
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.last_transaction = []
+  def add_item(self, title, price, quantity= 1):
+    self.total += price * quantity
+    for i in range(quantity):
+      self.items.append(title)
+      self.last_transaction.append({
+        "title": title, "quantity" : quantity, "price" : price
+      })        
